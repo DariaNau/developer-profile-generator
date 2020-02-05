c1d83f2c50d44fbbcc02366cdcc4ad2c55f2ed4f
Daria <dariagnaumova@gmail.com>
2020-02-05 14:39:01 -0500

    first api call functions added. Works fine

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Node.js and ES6
 Create a command-line application that dynamically generates a PDF profile from a GitHub username. The application will be invoked with the following command:
 node index.js
 The user will be prompted for a favorite color, which will be used as the background color for cards.
+
 # The PDF will be populated with the following:
 
 Profile image
 User name
+
 Links to the following:
 
 User location via Google Maps
--- index.js
+++ index.js
@@ -1,11 +1,115 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const pdf = require('html-pdf')
+
+const generateHTML = require("./generateHTML");
+
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const questions = [
-  
+  {
+    type: "input",
+    name: "username",
+    message: "What is your github username?"
+  },
+  {
+    type: "list",
+    name: "color",
+    message: "What is your favorite color?",
+    choices: ["green", "blue", "pink", "red"]
+  }
 ];
 
-function writeToFile(fileName, data) {
- 
+function firstCall() {
+  return new Promise(function(resolve, reject) {
+    inquirer.prompt(questions).then(async function(answers) {
+      const { username, color } = answers;
+      const queryUrl = `https://api.github.com/users/${username}`;
+      try {
+        const response = await axios.get(queryUrl);
+        resolve({
+          ...response.data,
+          color
+        });
+      } catch (error) {
+        reject(error);
+      }
+      // axios
+      //   .get(queryUrl)
+      //   .then(function(response) {
+      //     resolve({
+      //       ...response.data,
+      //       color
+      //     });
+      //   })
+      //   .catch(function(error) {
+      //     reject(error);
+      //   });
+    });
+  });
 }
 
-function init() {
+// firstCall()
+//   .then(function(data) {
+//     const html = generateHTML(data);
+//     console.log(html);
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
 
-init();
+ init()
+
+async function init() {
+  try {
+    const data = await firstCall();
+    const html = generateHTML(data);
+    await writeFileAsync('./profile.html', html)
+    pdf.create(html).toFile('./profile.pdf', function(err, res){
+      if(err) throw err;
+      console.log(res)
+    })
+    console.log(html);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//need to create a data object with response and color to pass to generateHTML?
+// let data = {
+//   username:
+//   color: questions.color
+// }
+
+// firstCall()
+//   .then(function(data) {
+//     const html = HTMLhelper.generateHTML(data);
+//     return writeFileAsync("index.html", JSON.stringify(html));
+//   })
+//   .then(function() {
+//     console.log("Successfully wrote to index.html");
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+
+// async function writeToFile(fileName, data) {
+//   try {
+//     await writeFileAsync(fileName, data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+// function init() {}
+// init();
+
+
+// function writeFileAsync(){
+//   return new Promise(function(resolve, reject){
+//     fs.writeFile("./dummy.txt", "Hello!", function(err) {
+//       if (err) reject(err);
+//     });
+//   })
+// }
